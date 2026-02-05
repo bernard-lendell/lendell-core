f58d90ae13afc4f88bb13ea42547f153897d2a4c
Bernard Gresola <btgresola@uerm.edu.ph>
2026-02-05 22:45:06 +0800

    Prototype for Endorsement Tracking

--- src/pages/TrackerPage.vue
+++ src/pages/TrackerPage.vue
@@ -1,35 +1,189 @@
 <template>
   <q-page class="row fit text-center q-pa-md">
     <div class="col-12">
-      <q-form @submit="this.testUpload()">
-        <q-file v-model="file" label="Upload File"></q-file>
-        <q-btn label="SUBMIT" type="submit"></q-btn>
-      </q-form>
+      <div class="q-pa-md">
+        <q-table
+          color="primary"
+          dense
+          card-class="bg-grey-2 text-grey-10"
+          flat
+          :filter="filter"
+          bordered
+          title="Treats"
+          :rows="rows"
+          :columns="columns"
+          row-key="name"
+          virtual-scroll
+          v-model:pagination="pagination"
+          :rows-per-page-options="[0]"
+        >
+          <template v-slot:top-left>
+            <div class="row items-center justify-between q-col-gutter-sm">
+              <div>ENDORSEMENTS</div>
+              <div>
+                <q-btn
+                  icon="fa fa-calendar"
+                  label="DATE: 2026-02-01 - 2026-02-05"
+                  color="primary"
+                  size="sm"
+                ></q-btn>
+              </div>
+            </div>
+          </template>
+          <template v-slot:top-right>
+            <q-input dense debounce="300" v-model="filter" placeholder="Search">
+              <template v-slot:append>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+          </template>
+
+          <template v-slot:header="props">
+            <q-tr :props="props">
+              <q-th auto-width />
+              <q-th v-for="col in props.cols" :key="col.name" :props="props">
+                {{ col.label }}
+              </q-th>
+            </q-tr>
+          </template>
+
+          <template v-slot:body="props">
+            <q-tr :props="props">
+              <q-td auto-width>
+                <q-btn
+                  size="xs"
+                  color="secondary"
+                  text-color="grey-2"
+                  round
+                  dense
+                  @click="props.expand = !props.expand"
+                  :icon="props.expand ? 'fa fa-minus' : 'fa fa-add'"
+                />
+              </q-td>
+              <q-td v-for="col in props.cols" :key="col.name" :props="props">
+                {{ col.value }}
+              </q-td>
+            </q-tr>
+            <q-tr v-show="props.expand" :props="props">
+              <q-td colspan="100%">
+                <div class="text-left">
+                  This is expand slot for row above: {{ props.row.name }}.
+                </div>
+              </q-td>
+            </q-tr>
+          </template>
+        </q-table>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { useUserStore } from 'src/stores/user'
-import { useCandidateStore } from 'src/stores/candidate'
 
 export default defineComponent({
   name: 'TestUpload',
   data() {
     return {
-      userStore: useUserStore(),
-      candidateStore: useCandidateStore(),
-      file: null,
+      filter: '',
+      pagination: {
+        rowsPerPage: 0,
+      },
+      columns: [
+        {
+          name: 'name',
+          required: true,
+          label: 'Dessert (100g serving)',
+          align: 'left',
+          field: (row) => row.name,
+          format: (val) => `${val}`,
+          sortable: true,
+        },
+        { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+        { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+        { name: 'protein', label: 'Protein (g)', field: 'protein' },
+        { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+        {
+          name: 'calcium',
+          label: 'Calcium (%)',
+          field: 'calcium',
+          sortable: true,
+          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+        },
+        {
+          name: 'iron',
+          label: 'Iron (%)',
+          field: 'iron',
+          sortable: true,
+          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+        },
+      ],
+      rows: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          sodium: 129,
+          calcium: '8%',
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          sodium: 337,
+          calcium: '6%',
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          sodium: 413,
+          calcium: '3%',
+          iron: '8%',
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          sodium: 327,
+          calcium: '7%',
+          iron: '16%',
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          sodium: 50,
+          calcium: '0%',
+          iron: '0%',
+        },
+      ],
     }
   },
   mounted() {},
-  methods: {
-    async testUpload() {
-      const formData = new FormData()
-      formData.append('file', this.file)
-      await this.candidateStore.testUpload(formData)
-    },
-  },
+  methods: {},
 })
 </script>
